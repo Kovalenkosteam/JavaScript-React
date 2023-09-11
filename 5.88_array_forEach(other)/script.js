1ba2acedde6456d318fe10e151bc195a5d6f7f97
kovalenkosteam <kovalenkosteam@gmail.com>
2023-09-11 22:39:53 +0300

    5.88_arr_sort+task

--- 5.88_array_forEach(other)/script.js
+++ 5.88_array_forEach(other)/script.js
@@ -42,14 +42,90 @@
 // console.log(res);
 
 
-const obj={
-	eugene:'persone',
-	Ann:'persone',
-	dog:'animal',
-	cat:'animal'
-};
+// const obj={
+// 	eugene:'persone',
+// 	Ann:'persone',
+// 	dog:'animal',
+// 	cat:'animal'
+// };
 
-const newarr=Object.entries(obj)
-.filter((item)=>item[1]==='persone')
-.map((item)=>item[0]);
-console.log(newarr);
+// const newarr=Object.entries(obj)
+// .filter((item)=>item[1]==='persone');
+// // .map((item)=>item[0]);
+// console.log(newarr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+
+
+//  Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+
+
+//  Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+
+// Пример:
+
+// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+
+// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray);
+
+
+
+const films = [
+	{
+		name: 'Titanic',
+		rating: 9
+	},
+	{
+		name: 'Die hard 5',
+		rating: 5
+	},
+	{
+		name: 'Matrix',
+		rating: 8
+	},
+	{
+		name: 'Some bad film',
+		rating: 4
+	}
+];
+
+// function showGoodFilms(arr) {
+// 	return arr.filter((item)=>item.rating>=8);
+// }
+// console.log(showGoodFilms(films));
+
+
+// function showListOfFilms(arr) {
+// 	return arr.reduce((sum,current)=>`${typeof(sum)==='object'?sum.name:sum},${current.name}`);
+// }
+// console.log(showListOfFilms(films));
+
+
+function setFilmsIds(arr) {
+	return arr.map((item, i) => {
+		item.id = i;
+		return item;
+	});
+}
+// console.log(setFilmsIds(films));
+
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+	return arr.every(item => item.id != 'undefined');
+}
+console.log(checkFilms(tranformedArray));
