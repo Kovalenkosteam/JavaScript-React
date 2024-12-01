ca377306a3481c16d9cd699b33e573c4a958a53d
kovalenkosteam <kovalenkosteam@gmail.com>
2024-12-02 02:48:03 +0300

    2.22-2.23

---
+++ 2.22/script.js
@@ -0,0 +1,58 @@
+// let num =50;
+
+// while(num<55){
+//     console.log(num);
+//     num++;
+// };
+// -----------------------------
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while (num<55);
+// ----------------------------
+// for(let i=1;i<10;i++){
+//     if(i===6){
+//         break;
+//     }
+//     console.log(i)
+// }
+// --------------------------
+// for(let i=1;i<10;i++){
+//     if(i===6){
+//         continue;
+//     }
+//     console.log(i)
+// }
+// ------------------------------
+// for (let i =0;i<3;i++){
+//     console.log(i);
+//     for(let j=0;j<3;j++){
+//         console.log(j);
+//     }
+// }
+// --------------------------
+// let result ='';
+// const length=7;
+// for(let i=1;i<length;i++){
+//     for(let j =0;j<i;j++){
+//         result+='*'
+//     };
+//     result+='\n';
+// }
+// console.log(result);
+
+// ----------------------------------
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`first lvl:${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`second lvl;${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if(k==2) break first;
+//             console.log(`third lvl:${k}`);
+//         }
+//     }
+// }
+
+// --------------------------
