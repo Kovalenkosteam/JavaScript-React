08dde242f93679cdf11418eb099e78a2dc8a5a06
kovalenkosteam <kovalenkosteam@gmail.com>
2024-12-02 15:00:38 +0300

    2.31

---
+++ 2.31/script.js
@@ -0,0 +1,21 @@
+function first() {
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+function second() {
+    console.log(2);
+}
+first();
+second();
+
+function learnJS(lang,callback){
+    console.log(`i learn:${lang}`)
+    callback();
+}
+
+function done(){
+    console.log('done')
+}
+
+learnJS('JS',done);
