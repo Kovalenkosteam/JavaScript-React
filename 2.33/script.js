e9f4024bf8ad0bff3dc24967f81cbadc3edfbde0
kovalenkosteam <kovalenkosteam@gmail.com>
2024-12-03 10:18:57 +0300

    2.33

---
+++ 2.33/script.js
@@ -0,0 +1,39 @@
+const arr=[1,23,45,7,2,3,79];
+
+// console.log(arr.sort(compareNum));
+// function compareNum(a,b){
+//     return a-b
+// }
+
+// console.log(arr);
+// arr.pop(); //удаляет последний элемент
+
+// console.log(arr);
+// arr.push('log'); //добавляет элемент в конец массива
+
+// console.log(arr);
+// arr.shift(); //удаляет первый элемент
+
+// console.log(arr);
+// arr.unshift('first'); //добавляет элемент в начало массива
+
+// console.log(arr);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// for(let value of arr){
+//     console.log(value);
+// }
+
+
+// arr.forEach(function(item,index,arr){
+//     console.log(`${index}: ${item} внутри массива ${arr}`);
+// })
+
+
+// const str='1243, sdf, 3, 35, 2';
+// const product = str.split(", ") //создание массива из строки
+// console.log(product.sort())//сортировка по порядку
+// console.log(arr.join('; '))//из массива в строку 
